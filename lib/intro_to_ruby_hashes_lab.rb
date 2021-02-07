b7677b0da9ed864847ec21381d20d61b2432e76f
Yuri Ng <yuringuyen312@gmail.com>
2021-02-07 03:27:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,15 @@ end
 read_from_hash({name:"Yuri"}, :name)
 
 def update_counting_hash(hash, key)
+  if !hash[key]
+    hash[key] = 1
+  else
+    hash[key] +=1
+
+  end
+ p hash
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 end
+  update_counting_hash({numer: 1}, :age)
